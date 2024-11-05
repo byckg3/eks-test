f81e25d61163ed5ebd49bfdde5c40ca4a9b52cd0
byc <byckg3@gmail.com>
2024-11-06 00:41:45 +0800

    modified output fields

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "cluster_name" {
+output "aws_region" {
+  description = "AWS region"
+  value       = var.aws_region
+}
+
+output "eks_cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
 
-output "cluster_endpoint" {
+output "eks_cluster_endpoint" {
   description = "Endpoint for EKS control plane"
   value       = module.eks.cluster_endpoint
 }
