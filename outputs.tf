7c5418538ed1acc9a7cc258684fd17bf4c01b644
byc <byckg3@gmail.com>
2024-11-16 01:49:28 +0800

    add output value

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,7 @@ output "eks_cluster_endpoint" {
   description = "Endpoint for EKS control plane"
   value       = module.eks.cluster_endpoint
 }
+
+output "eks_oidc_provider_arn" {
+  value       = module.eks.oidc_provider_arn
+}
